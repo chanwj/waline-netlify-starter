9d9ff48ed0c7cf14feca84673c7d01ad2f7ed2c5
小橘猫 <chan.wj@outlook.com>
2025-06-18 20:05:51 +0800

    Update comment.js
    Add qq match

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -7,6 +7,22 @@ const app = Waline({
   async postSave(comment) {
     // do what ever you want after save comment
   },
+  // 通过QQ号或者QQ邮箱获取QQ头像
+  async avatarUrl(comment) {
+    const regqq = new RegExp('^[1-9][0-9]{4,11}$');
+    const regqqmail = new RegExp('(\\d+)@qq\\.com$', 'i');
+    const mail = comment.mail;
+    const nick = comment.nick;
+    if (regqq.test(nick)) {
+      return 'https://q1.qlogo.cn/headimg_dl?dst_uin=' + nick + '&spec=4';  
+    }
+    if (regqqmail.test(mail)) {
+      const qq = mail.replace(/@qq\.com/i, '').toLowerCase();  
+      if (regqq.test(qq)) {
+        return 'https://q1.qlogo.cn/headimg_dl?dst_uin=' + qq + '&spec=4';
+      }
+    }
+  },
 });
 
 module.exports.handler = serverless(http.createServer(app));
